fa4cd1152cf7efc769a4c476fa2f126557680469
dabhade904 <dabhade904@gmail.com>
2022-07-28 23:31:06 +0530

    Added : fuctionality to calculating employee wages for a month

--- EmplyeeWage.cs
+++ EmplyeeWage.cs
@@ -17,27 +17,31 @@ namespace EmployeeWageProblem
         public  void calculateEmployeeWage()
         {
             //Constant
-            int WAGE_PER_HRS = 20;
-            Random obj = new Random();
-            //variable
-            int empCheck=obj.Next(3);
-            int workingHours = 0;
-            switch (empCheck)
-              {
-                  case 0:
+            int WAGE_PER_HRS = 20,WORKING_DAY_PER_MONTH=20;
+            int totalWage = 0;
+            for (int i = 1; i < WORKING_DAY_PER_MONTH; i++)
+            {
+                Random obj = new Random();
+                //variable
+                int empCheck = obj.Next(3);
+                int workingHours = 0;
+                switch (empCheck)
+                {
+                    case 0:
                         workingHours = 8;
-                        Console.WriteLine("full day wage is :"+workingHours );
-                      break;
-                  case 1:
+                         break;
+                    case 1:
                         workingHours = 4;
-                        Console.WriteLine("part time wage is : "+workingHours);
-                      break ;
-                  default:
+                        break;
+                    default:
                         Console.WriteLine("employee is absent");
-                      break;
-              }
-            int wage = workingHours * WAGE_PER_HRS;
-            Console.WriteLine("employee wages are "+wage);
+                        break;
+                }
+                int wage = workingHours * WAGE_PER_HRS;
+                Console.WriteLine("day " +i+" wage is " + wage);
+                totalWage += wage;
+            }
+            Console.WriteLine("total wage for a month is " + totalWage);
         }
     }
 }
