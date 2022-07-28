efa07bdb6be3ac4b45adb12b3d1f4052d21be23c
dabhade904 <dabhade904@gmail.com>
2022-07-28 10:43:35 +0530

    Added : initialise the project and added welcome message

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace EmployeeWageProblem
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Welcome to employee wage problem");
+        }
+    }
+}
+
