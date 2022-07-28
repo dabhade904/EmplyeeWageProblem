b4d2b1962ddff124a2c2e4c0295383b3532acb0b
dabhade904 <dabhade904@gmail.com>
2022-07-28 17:07:26 +0530

    Added : functionality to calculate part time employee wage

--- EmplyeeWage.cs
+++ EmplyeeWage.cs
@@ -17,16 +17,19 @@ namespace EmployeeWageProblem
         public static void calculateEmployeeWage()
         {
             //Constant
-            int IS_EMP_PRESENT = 1;
-            int WAGE_PER_HRS = 20, FLL_DAY_HRS = 8;
+            int IS_FLL_DAY = 0,IS_HALF_DAY=1;
+            int WAGE_PER_HRS = 20, FLL_DAY_HRS = 8,PART_TIME_WAGE=4;
             Random obj = new Random();
             //variable
-            int empCheck=obj.Next(2);
-            if (empCheck == IS_EMP_PRESENT)
+            int empCheck=obj.Next(3);
+            if (empCheck == IS_FLL_DAY)
             {
-                Console.WriteLine("employee is present");
-                int wage = WAGE_PER_HRS * FLL_DAY_HRS;
-                Console.WriteLine(wage);
+                int dailyWage = WAGE_PER_HRS * FLL_DAY_HRS;
+                Console.WriteLine("full day wage is :" + dailyWage);
+            }else if (empCheck == IS_HALF_DAY)
+            {
+                int dailyWage = WAGE_PER_HRS * PART_TIME_WAGE;
+                Console.WriteLine("part time wage is : "+dailyWage);
             }
             else
             {
