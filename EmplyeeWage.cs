60878690d84aa525827b18c2f0b9c487d065b7ae
dabhade904 <dabhade904@gmail.com>
2022-08-08 20:48:41 +0530

    Added: function to manage wage of Multiple Companies

--- EmplyeeWage.cs
+++ EmplyeeWage.cs
@@ -14,54 +14,58 @@ namespace EmployeeWageProblem
         /// 
         public const int IS_FULL_TIME = 0;
         public const int IS_PART_TIME = 1;
-        private string company;
-        private int ratePerHrs;
-        private int numberOfWorkingDay;
-        private int totalWorkingHrs;
-        private int totalWage;
 
-        public EmplyeeWage(string company,int ratePerHrs,int numberOfWorkingDay,int totalWorkingHrs)
+        private int numOfCompany = 0;
+        private CompanyEmpWage[] companyEmpWageArray;
+
+        public EmplyeeWage()
         {
-            this.company = company;
-            this.ratePerHrs = ratePerHrs;
-            this.numberOfWorkingDay = numberOfWorkingDay;
-            this.totalWorkingHrs=totalWorkingHrs;
+            this.companyEmpWageArray = new CompanyEmpWage[5];
         }
 
-   
-         public  void calculateEmployeeWage()
-         {
-             //variable
-             int totalEmpHrs = 0;
-             int totalWorkingDay = 0;
-             int workingHours ;
-             while (totalEmpHrs != this.totalWorkingHrs && totalWorkingDay != this.numberOfWorkingDay)
-             {
-                 totalWorkingDay++;
-                 Random obj = new Random();
-                 int empCheck = obj.Next(3);
-                 switch (empCheck)
-                 {
-                     case IS_FULL_TIME:
-                         workingHours = 8;
-                          break;
-                     case IS_PART_TIME:
-                         workingHours = 4;
-                         break;
-                     default:
-                         workingHours = 0;
-                         break;
-                 }
-                 totalEmpHrs += workingHours;  
-                 Console.WriteLine("day "+totalWorkingDay+" wage is " + workingHours);
-             }
-             totalWage = totalEmpHrs * ratePerHrs;
-             Console.WriteLine("total wage for company "+company+" is " + totalWage);
-         }
-        public string toString()
+        public void addCompanyEmpWage(string company, int ratePerHrs, int numberOfWorkingDay, int totalWorkingHrs)
         {
-            return "Total emp wage for company : " + this.company + " is: " + this.totalWage;
+            companyEmpWageArray[this.numOfCompany] = new CompanyEmpWage(company, ratePerHrs, numberOfWorkingDay, totalWorkingHrs);
+            numOfCompany++;
         }
+
+        public void computeWage()
+        {
+            for (int i = 0; i < numOfCompany; i++)
+            {
+                companyEmpWageArray[i].setTotalEmpWage(this.calculateEmployeeWage(this.companyEmpWageArray[i]));
+                Console.WriteLine(this.companyEmpWageArray[i].toString());
+            }
+        }
+        private int calculateEmployeeWage(CompanyEmpWage companyEmpWage)
+        {
+            //variable
+            int totalEmpHrs = 0;
+            int totalWorkingDay = 0;
+            int workingHours;
+            while (totalEmpHrs != companyEmpWage.totalWorkingHrs && totalWorkingDay != companyEmpWage.numberOfWorkingDay)
+            {
+                totalWorkingDay++;
+                Random obj = new Random();
+                int empCheck = obj.Next(3);
+                switch (empCheck)
+                {
+                    case IS_FULL_TIME:
+                        workingHours = 8;
+                        break;
+                    case IS_PART_TIME:
+                        workingHours = 4;
+                        break;
+                    default:
+                        workingHours = 0;
+                        break;
+                }
+                totalEmpHrs += workingHours;
+                Console.WriteLine("day " + totalWorkingDay + " wage is " + workingHours);
+            }
+            return totalEmpHrs * companyEmpWage.ratePerHrs;
+        }
+
     }
 }
 
